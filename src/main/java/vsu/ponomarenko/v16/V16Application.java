97bcd98a88173ef03265c92d03bd8b2bc7e758c2
DmitriiPonomarenko <90408762+DmitriiPonomarenko@users.noreply.github.com>
2022-06-10 23:27:21 +0300

    Initial commit

---
+++ src/main/java/vsu/ponomarenko/v16/V16Application.java
@@ -0,0 +1,13 @@
+package vsu.ponomarenko.v16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V16Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(V16Application.class, args);
+    }
+
+}
---
+++ src/test/java/vsu/ponomarenko/v16/V16ApplicationTests.java
@@ -0,0 +1,13 @@
+package vsu.ponomarenko.v16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V16ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
